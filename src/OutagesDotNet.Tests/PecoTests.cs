40f9f93e5c5d56451867421ca51f9893b5694f9e
thegreatco <thegreatco@thegreatco.com>
2013-03-13 16:06:31 -0400

    Renamed Tests, Added PECO provider.

---
+++ src/OutagesDotNet.Tests/PecoTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using OutagesDotNet.Peco;
+
+namespace OutagesDotNet.Tests
+{
+    /// <summary>
+    /// The peco tests.
+    /// </summary>
+    [TestFixture]
+    class PecoTests
+    {
+        /// <summary>
+        /// Outputs just the currently listed outages.
+        /// </summary>
+        [Test]
+        public async void OutagesOnlyBucks()
+        {
+            var stuff = new Provider();
+            var outages = await stuff.GetCurrentData(ServiceArea.Bucks);
+            foreach (var outage in outages.Where(x => x.Out > 0))
+            {
+                Console.WriteLine("{0}: {1}", outage.City, outage.Out);
+            }
+        }
+
+        /// <summary>
+        /// Outputs just the currently listed outages.
+        /// </summary>
+        [Test]
+        public async void OutagesAll()
+        {
+            var stuff = new Provider();
+            foreach (ServiceArea state in Enum.GetValues(typeof(ServiceArea)))
+            {
+                var outages = await stuff.GetCurrentData(state);
+                foreach (var outage in outages.Where(x => x.Out > 0))
+                {
+                    Console.WriteLine("{0}: {1}", outage.City, outage.Out);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Outputs all the data returned by the provider.
+        /// </summary>
+        [Test]
+        public async void AllDataBucks()
+        {
+            var stuff = new Provider();
+            var outages = await stuff.GetCurrentData(ServiceArea.Bucks);
+            foreach (var outage in outages)
+            {
+                if (outage.Served > 0)
+                {
+                    double percentOut = ((double)outage.Out / (double)outage.Served) * 100;
+                    Console.WriteLine("{4}{0}: {1} of {2} ({3}%)", outage.City, outage.Out, outage.Served, percentOut, percentOut > 0d ? "----> " : string.Empty);
+                }
+                else Console.WriteLine("{0}: {1} of {2}", outage.City, outage.Out, outage.Served);
+            }
+        }
+
+        /// <summary>
+        /// Outputs all the data returned by the provider.
+        /// </summary>
+        [Test]
+        public async void AllDataAll()
+        {
+            var stuff = new Provider();
+            foreach (ServiceArea state in Enum.GetValues(typeof(ServiceArea)))
+            {
+                Console.WriteLine("Getting Information for {0}", state);
+                var outages = await stuff.GetCurrentData(state);
+                foreach (var outage in outages)
+                {
+                    if (outage.Served > 0)
+                    {
+                        double percentOut = ((double)outage.Out / (double)outage.Served) * 100;
+                        Console.WriteLine("{4}{0}: {1} of {2} ({3}%)", outage.City, outage.Out, outage.Served, percentOut, percentOut > 0d ? "----> " : string.Empty);
+                    }
+                    else Console.WriteLine("{0}: {1} of {2}", outage.City, outage.Out, outage.Served);
+                }
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(string.Empty);
+            }
+        }
+    }
+}
--- src/OutagesDotNet/Peco/Provider.cs
+++ src/OutagesDotNet/Peco/Provider.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+
+using Newtonsoft.Json;
 
 namespace OutagesDotNet.Peco
 {
@@ -15,10 +21,40 @@ namespace OutagesDotNet.Peco
         /// </summary>
         /// <param name="sa"> The service area. </param>
         /// <returns> An await-able <see cref="Task"/> containing the outage results. </returns>
-        public Task<IEnumerable<Outage>> GetCurrentData(ServiceArea sa)
+        public async Task<IEnumerable<Outage>> GetCurrentData(ServiceArea sa)
         {
-            // https://www.peco.com/CustomerService/OutageCenter/OutageMap/Pages/CountyDetails.aspx?County=Bucks
-            return null;
+            var client = new HttpClient();
+            const string Url = "https://www.peco.com/CustomerService/OutageCenter/OutageMap/Pages/CountyDetails.aspx?County={0}";
+            var uri = new Uri(string.Format(Url, sa));
+            var htmlStream = await client.GetStreamAsync(uri);
+            return ParseResults(htmlStream, sa);
+        }
+
+        private static IEnumerable<Outage> ParseResults(Stream htmlStream, ServiceArea sa)
+        {
+            var outages = new List<Outage>();
+            var doc = new HtmlDocument();
+            doc.Load(htmlStream);
+            foreach (var tables in doc.DocumentNode.SelectNodes("//div[contains(@id,'outageMapTownshipInformation')]//table").Where(x => x.SelectNodes("thead") != null))
+            {
+                // if (tables.SelectNodes("thead") == null) continue;
+                foreach (var node in tables.SelectNodes("tbody/tr"))
+                {
+                    var subNodes = node.SelectNodes("td");
+                    int custsOut;
+                    int.TryParse(subNodes[1].InnerText, out custsOut);
+                    var outage = new Outage
+                        {
+                            City = subNodes[0].InnerText,
+                            State = "Pennsylvania",
+                            County = sa.ToString(),
+                            Out = custsOut,
+                            Served = null
+                        };
+                    outages.Add(outage);
+                }
+            }
+            return outages;
         }
     }
 }
